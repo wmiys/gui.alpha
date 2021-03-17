9d631add3e5dc7bebbbb2395fc6cfa6e1ca23897
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-17 18:22:55 -0500

    add initial files

---
+++ src/home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include('header.php'); ?>
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    
+    <div class="div">
+
+        <h1 class="my-5 text-center">WMIYS</h1>
+    
+    </div>
+
+
+<?php include('footer.php'); ?>
+<script src="js/home.js"></script>
+
+</body>
+
+</html>
