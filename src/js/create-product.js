b786847ec6b06c02a8fe08f4e4e1d849a0993ab7
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-23 20:38:06 -0500

    create module variables

--- src/js/create-product.js
+++ src/js/create-product.js
@@ -1,10 +1,40 @@
+/**********************************************************
+Module variables
+**********************************************************/
+const eForm = $('.form-new-product');
 
+// tab pages
+const ePages = {
+    category   : $('#form-new-product-page-category'),
+    location   : $('#form-new-product-page-location'),
+    renterInfo : $('#form-new-product-page-renter-info'),
+    photos     : $('#form-new-product-page-photos'),
+    nameDesc   : $('#form-new-product-page-name-desc'),
+    price      : $('#form-new-product-page-price'),
+}
+
+
+// form inputs
+const eInputs = {
+    categoryMajor : $('#form-new-product-input-category-major'),
+    categoryMinor : $('#form-new-product-input-category-minor'),
+    categorySub   : $('#form-new-product-input-category-sub'),
+    location      : $('#form-new-product-input-location'),
+    photos        : $('#form-new-product-input-photos'),
+    name          : $('#form-new-product-input-name'),
+    description   : $('#form-new-product-input-description'),
+    priceFull     : $('#form-new-product-input-price-full'),
+    priceHalf     : $('#form-new-product-input-price-half'),
+}
+
+
+const cInputs = '.form-new-product-input';
 
 /**********************************************************
 Main logic
 **********************************************************/
 $(document).ready(function() {
-    //logic goes here...
+    
 });
 
 
