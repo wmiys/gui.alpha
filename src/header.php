7bf755cf4c9d1d81eb78020c6480b6916a8f8404
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-29 23:17:28 -0500

    start figuring out product cards

--- src/header.php
+++ src/header.php
@@ -5,6 +5,8 @@
 <!-- bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
 
+<link rel="stylesheet" href="css/external/directory.css" />
+
 <!-- boxicons -->
 <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
 
@@ -18,7 +20,6 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <!-- select2 bootstrap4 theme: https://github.com/ttskch/select2-bootstrap4-theme -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
-
 
 <!-- personal css -->
 <link rel="stylesheet" type="text/css" href="css/style.css" />
--- src/products.php
+++ src/products.php
@@ -9,16 +9,45 @@
 <body>
     <?php include('navbar.php'); ?>
     <div class="container">
-
         <h1 class="my-5 text-center">Your products</h1>
 
-        
+
+        <div class="user-products">
+            <div class="card card-user-product">
+                <img src="img/placeholder.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h6 class="card-title text-dark">Name of the product</h6>
+                    <p class="text-muted text-small">Sub category</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     </div>
 
 
+
+
+
     <?php include('footer.php'); ?>
-    <!-- <script src="js/home.js"></script> -->
+    <script src="js/products.js"></script>
 
 </body>
 
