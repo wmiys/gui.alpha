6e3ed825537f4aab77ed4760bcd8d2f0973ca653
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-29 22:39:00 -0500

    update the navbar

--- src/navbar.php
+++ src/navbar.php
@@ -1,16 +1,19 @@
 <section id="navbar">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
                 <i class='bx bx-menu'></i>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link custom-font" id="nav-item-home" href="home.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link custom-font" id="nav-item-new-event" href="login.php">Login</a></li>
-                    <li class="nav-item"><a class="nav-link custom-font" id="nav-item-logout" href="create-account.php">Create Account</a></li>
-                    <li class="nav-item"><a class="nav-link" id="nav-item-test" href="create-product.php">New Product</a></li>
+                    <li class="nav-item"><a class="nav-link" id="nav-item-test" href="products.php">Your products</a></li>
+                </ul>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link custom-font float-right" id="nav-item-new-event" href="login.php">Logout</a></li>
                 </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 </section>
-
