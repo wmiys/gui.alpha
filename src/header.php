33ac89e896ea19d8d2e86abe26537d977e306802
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-30 18:59:49 -0500

    remove remove directory css and fix navbar

--- src/header.php
+++ src/header.php
@@ -5,7 +5,7 @@
 <!-- bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
 
-<link rel="stylesheet" href="css/external/directory.css" />
+<!-- <link rel="stylesheet" href="css/external/directory.css" /> -->
 
 <!-- boxicons -->
 <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
