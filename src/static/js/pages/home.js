fcecaf4a3fb3e1ad48d23a19eea67e5ca8f746aa
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-04-28 21:51:19 -0500

    layout starting input elements

--- src/static/js/pages/home.js
+++ src/static/js/pages/home.js
@@ -1,5 +1,103 @@
 
-// LocalStorage.validateStatus();  // be sure the user is logged in
+
+const eFormProductSearch = {
+
+    form: '#product-search-form',
+
+    inputs: {
+        location: '#product-search-form-input-location',
+        dates: '#product-search-form-input-dates',
+        category: '#product-search-form-input-catgory',
+    },
+
+    dropdownDataAttr = 'data-product-category-sub-id',
+
+    classes: {
+        input: '.product-search-form-input',
+    },
+
+
+
+    getInputValues: function() {
+        let values = {};
+        values.location_id = $(eFormProductSearch.inputs.location).val();
+        values.dates = $(eFormProductSearch.inputs.location).val();
+        values.product_categories_sub_id = $(eFormProductSearch.inputs.location).val();
+
+        return values;
+    },
+
+
+    getProductCategorySubId: function() {
+        const result = $(eFormProductSearch.category).attr(eFormProductSearch.dropdownDataAttr);
+        return result;
+    },
+}
+
+
+
+
+
+/**********************************************************
+Main logic
+**********************************************************/
+$(document).ready(function() {
+
+    loadSelect2();
+});
+
+
+
+/**********************************************************
+Loads the select2 library on the location input
+**********************************************************/
+function loadSelect2() {
+    $(eInputs.location).select2({
+        minimumInputLength: 3,
+        theme: 'bootstrap4',
+        ajax: {
+            delay: 150,
+            url: ApiWrapper.URLS.SEARCH.LOCATIONS,
+            placeholder: "Select a state",
+            allowClear: true,
+            data: function (params) {
+                const urlParms = {      // set the request url ?parms
+                    q: params.term,
+                }                
+                return urlParms;
+            },
+            processResults: function (data) {
+                const processedResults = processLocationSearchApiResponse(data);
+                return processedResults;
+            }
+        },
+        
+    });
+}
+
+/**********************************************************
+Process the api response data for the location search request.
+It is transformed into the recognized format for select2.
+**********************************************************/
+function processLocationSearchApiResponse(apiResponse) {
+    let processedData = [];
+    for (let count = 0; count < apiResponse.length; count++) {
+        const location = apiResponse[count];
+        const text = `${location.city}, ${location.state_name}`;
+        processedData.push({id: location.id, text: text});
+    }
+    
+    return ({results: processedData});
+}
+
+
+
+
+
+
+
+
+
 
 
 
