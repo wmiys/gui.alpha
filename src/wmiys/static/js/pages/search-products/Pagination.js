43637d746acbceeb52effe6c162c602d6901f322
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-05-13 07:58:09 -0500

    move the pagination logic into its own class

---
+++ src/wmiys/static/js/pages/search-products/Pagination.js
@@ -0,0 +1,126 @@
+
+
+class Pagination
+{
+
+    /**********************************************************
+    Constructor.
+
+    Parms:
+        a_eList (str) - string for jquery to select the element.
+    **********************************************************/
+    constructor(a_eList) {
+        if (a_eList == undefined || a_eList == null) {
+            a_eList = Pagination.list;
+        }
+
+        this.eList = a_eList;
+
+        this.togglePaginationStates = this.init.bind(this);
+        this.togglePaginationStateFirstPage = this.togglePaginationStateFirstPage.bind(this);
+        this.toggleFirstAndOnlyPage = this.toggleFirstAndOnlyPage.bind(this);
+        this.togglePaginationStateLastPage = this.togglePaginationStateLastPage.bind(this);
+        this.togglePaginationStateNormal = this.togglePaginationStateNormal.bind(this);
+    }
+
+    /**********************************************************
+    Toggle the appropriate pagination state.
+    **********************************************************/
+    init() {
+        const presentPage = UrlParser.getQueryParm('page');
+        const lastPage = $(e_pagination.list).attr('data-page-last');
+
+        if (presentPage == 1 || presentPage == null) {
+            this.togglePaginationStateFirstPage();
+    
+            if (presentPage == lastPage) {
+                this.toggleFirstAndOnlyPage();
+            }
+    
+            return;
+            
+        } else if (presentPage == lastPage) {
+            this.togglePaginationStateLastPage();
+            return;
+        } else {
+            this.togglePaginationStateNormal();
+        }
+    }
+
+    /**********************************************************
+    First page action
+    **********************************************************/
+    togglePaginationStateFirstPage() {
+        let ul = $(this.eList);
+        const items = Pagination.items;
+    
+        // disable the first and previous buttons
+        $(ul).find(items.first).addClass('disabled');
+        $(ul).find(items.previous).addClass('disabled');
+    
+        // set the left button to active
+        $(ul).find(items.left).addClass('active');
+    
+        // put href value of the right into the middle one
+        const nextLink = $(ul).find(items.next).find('a').attr('href');
+        $(ul).find(items.center).find('a').attr('href', nextLink).text('2');
+    
+        // disable the right button
+        $(ul).find(items.right).find('a').text('...').closest('li').addClass('disabled');
+    }
+
+    /**********************************************************
+    First page and the only page action
+    **********************************************************/
+    toggleFirstAndOnlyPage() {
+        let ul = $(this.eList);
+        const items = Pagination.items;
+    
+        // disable the first and previous buttons
+        $(ul).find(items.center).addClass('disabled');
+        $(ul).find(items.next).addClass('disabled');
+        $(ul).find(items.last).addClass('disabled');
+    }
+
+    /**********************************************************
+    Last page action
+    **********************************************************/
+    togglePaginationStateLastPage() {
+        let ul = $(this.eList);
+        const items = Pagination.items;
+    
+        // disable the first and previous buttons
+        $(ul).find(items.last).addClass('disabled');
+        $(ul).find(items.next).addClass('disabled');
+        $(ul).find(items.right).addClass('disabled').find('a').text('...');
+    
+        // set the right button to active
+        $(ul).find(items.center).addClass('active');
+    }
+
+    /**********************************************************
+    Normal page.
+    **********************************************************/
+    togglePaginationStateNormal() {
+        $(this.eList).find(Pagination.items.center).addClass('active');
+    }
+
+
+}
+
+
+/**********************************************************
+Static properties
+**********************************************************/
+
+Pagination.list = '.pagination-search-products';
+
+Pagination.items = {};
+Pagination.items.first    = '.page-item-first';
+Pagination.items.previous = '.page-item-previous';
+Pagination.items.left     = '.page-item-left';
+Pagination.items.center   = '.page-item-center';
+Pagination.items.right    = '.page-item-right';
+Pagination.items.next     = '.page-item-next';
+Pagination.items.last     = '.page-item-last';
+
--- src/wmiys/static/js/pages/search-products/results.js
+++ src/wmiys/static/js/pages/search-products/results.js
@@ -1,104 +1,17 @@
 
 const m_SidenavFilterCategories = new SidenavFilterCategories();
 const m_SortingElement = new SortingElement();
+const m_Pagination = new Pagination(Pagination.list);
 
-
-const e_pagination = {
-    list: '.pagination-search-products',
-
-    items: {
-        first: '.page-item-first',
-        previous: '.page-item-previous',
-        left: '.page-item-left',
-        center: '.page-item-center',
-        right: '.page-item-right',
-        next: '.page-item-next',
-        last: '.page-item-last',
-    }
-};
 
 /**********************************************************
 Main logic
 **********************************************************/
 $(document).ready(function() {
-    // addListeners();
     m_SortingElement.displayOptions();
-    togglePaginationStates();
-
+    m_Pagination.init();
     toggleSidenavCollapse();
 });
-
-
-function togglePaginationStates() {
-    const presentPage = UrlParser.getQueryParm('page');
-    const lastPage = $(e_pagination.list).attr('data-page-last');
-
-    if (presentPage == 1 || presentPage == null) {
-        togglePaginationStateFirstPage();
-
-        if (presentPage == lastPage) {
-            toggleFirstAndOnlyPage();
-        }
-
-        return;
-        
-    } else if (presentPage == lastPage) {
-        togglePaginationStateLastPage();
-        return;
-    } else {
-        togglePaginationStateNormal();
-    }
-}
-
-function togglePaginationStateFirstPage() {
-
-    let ul = $(e_pagination.list);
-    const items = e_pagination.items;
-
-    // disable the first and previous buttons
-    $(ul).find(items.first).addClass('disabled');
-    $(ul).find(items.previous).addClass('disabled');
-
-    // set the left button to active
-    $(ul).find(items.left).addClass('active');
-
-    // put href value of the right into the middle one
-    const nextLink = $(ul).find(items.next).find('a').attr('href');
-    $(ul).find(items.center).find('a').attr('href', nextLink).text('2');
-
-    // disable the right button
-    $(ul).find(items.right).find('a').text('...').closest('li').addClass('disabled');
-}
-
-function toggleFirstAndOnlyPage() {
-    let ul = $(e_pagination.list);
-    const items = e_pagination.items;
-
-    // disable the first and previous buttons
-    $(ul).find(items.center).addClass('disabled');
-    $(ul).find(items.next).addClass('disabled');
-    $(ul).find(items.last).addClass('disabled');
-}
-
-
-function togglePaginationStateLastPage() {
-    let ul = $(e_pagination.list);
-    const items = e_pagination.items;
-
-    // disable the first and previous buttons
-    $(ul).find(items.last).addClass('disabled');
-    $(ul).find(items.next).addClass('disabled');
-    $(ul).find(items.right).addClass('disabled').find('a').text('...');
-
-    // set the right button to active
-    $(ul).find(items.center).addClass('active');
-}
-
-
-function togglePaginationStateNormal() {
-    $(e_pagination.list).find(e_pagination.items.center).addClass('active');
-}
-
 
 
 function toggleSidenavCollapse() {
