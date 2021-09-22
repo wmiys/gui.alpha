35d492c84c8afdcb4ff2d0404483aa42eacc9c2c
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-09-21 20:44:52 -0500

    setup the page

---
+++ src/coming-soon/main.js
@@ -0,0 +1,31 @@
+
+
+
+$(document).ready(function() {
+
+    let height = window.screen.height;
+
+
+
+    // setHeight(height);
+
+    // $(window.screen.height).on('change', function() {
+    //     console.log(window.screen.height);
+    // });
+
+});
+
+
+function setHeight(size) {
+    // // $('.card-home-search-hero').height(size);
+
+    // let subHeight = $('#text-container').height();
+    // console.log(subHeight);
+
+    // const height = `${size}px`;
+    // // const height = `${size - subHeight}px`;
+    // console.log(height);
+
+    // // document.querySelector('.card-home-search-hero').style.height = height;
+    // document.querySelector('.card-img').style.height = height;
+}
