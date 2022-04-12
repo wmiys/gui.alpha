fab4e8584779f6036b61a10300ee6b4e5e052c5a
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-04-11 19:55:35 -0500

    display a table

--- src/wmiys/static/js/rollup.config.js
+++ src/wmiys/static/js/rollup.config.js
@@ -30,7 +30,7 @@ const configs = [
     new RollupConfig('pages/product-listings/product-listing.js', 'dist/product-listings-product-listing.bundle.js'),
     new RollupConfig('pages/products/inventory.js', 'dist/products-inventory.bundle.js'),
     new RollupConfig('pages/products/overview.js', 'dist/products-overview.bundle.js'),
-    new RollupConfig('pages/products/requests.js', 'dist/products-requests.bundle.js'),
+    new RollupConfig('pages/products/requests/main.js', 'dist/products-requests.bundle.js'),
     new RollupConfig('pages/products/product/availability.js', 'dist/products-product-availability.bundle.js'),
     new RollupConfig('pages/products/product/insights.js', 'dist/products-product-insights.bundle.js'),
     new RollupConfig('pages/products/product/overview/main.js', 'dist/products-product-overview.bundle.js'),
